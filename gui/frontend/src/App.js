a8bac62549f4e545b76de9b045925fb9a54a626f
Michael Overton <m.overtonbrown@gmail.com>
2021-08-05 20:29:31 -0700

    more prototyping

--- gui/frontend/src/App.js
+++ gui/frontend/src/App.js
@@ -1,13 +1,43 @@
 import "./App.css";
+import axios from "axios";
 import { useState } from "react";
 import { Button, Row, Col, Container, Dropdown } from "react-bootstrap";
+
+const backendUrl = "http://localhost:4000";
 
 function App() {
   const [key, setKey] = useState("C");
   const [octaveCount, setOctaveCount] = useState(2);
+  const [root, setRoot] = useState(3);
+
+  const updateRoot = async () => {
+    const rootNote = key + root;
+    try {
+      await axios.get(`${backendUrl}/updateRoot?root=${rootNote}`);
+      // await fetch(`${backendUrl}/updateRoot?root=${rootNote}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const playNote = async () => {
+    try {
+      await axios.get(`${backendUrl}/play`);
+      // await fetch(`${backendUrl}/play`);
+      console.log("successfully fetched");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Container className="App my-5 mx-5">
+      <Row className="my-2 h2">
+        <Col>
+          <b>POWER GLOVE</b>
+        </Col>
+      </Row>
+
       <Row className="my-2">
         <Col>
           <Button>Power</Button>
@@ -21,26 +51,57 @@ function App() {
       </Row>
 
       <Row className="my-2">
-        <Col>Key: {key}</Col>
-        <Col>Octaves: {octaveCount}</Col>
-      </Row>
-
-      <Row className="my-2">
         <Col>
           <Dropdown>
-            <Dropdown.Toggle>Select Key</Dropdown.Toggle>
+            <Dropdown.Toggle>Key: {key}</Dropdown.Toggle>
 
             <Dropdown.Menu>
-              <Dropdown.Item onClick={() => setKey("C")}>C</Dropdown.Item>
-              <Dropdown.Item onClick={() => setKey("C#")}>C#</Dropdown.Item>
-              <Dropdown.Item onClick={() => setKey("D")}>D</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setKey("C");
+                  updateRoot();
+                }}
+              >
+                C
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setKey("C#");
+                  updateRoot();
+                }}
+              >
+                C#
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  setKey("D");
+                  updateRoot();
+                }}
+              >
+                D
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         </Col>
 
         <Col>
           <Dropdown>
-            <Dropdown.Toggle>Octave Count</Dropdown.Toggle>
+            <Dropdown.Toggle>Root: {root}</Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => setRoot(1)}>1</Dropdown.Item>
+              <Dropdown.Item onClick={() => setRoot(2)}>2</Dropdown.Item>
+              <Dropdown.Item onClick={() => setRoot(3)}>3</Dropdown.Item>
+              <Dropdown.Item onClick={() => setRoot(4)}>4</Dropdown.Item>
+              <Dropdown.Item onClick={() => setRoot(5)}>5</Dropdown.Item>
+              <Dropdown.Item onClick={() => setRoot(6)}>6</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle>Octaves: {octaveCount}</Dropdown.Toggle>
 
             <Dropdown.Menu>
               <Dropdown.Item onClick={() => setOctaveCount(1)}>1</Dropdown.Item>
@@ -49,6 +110,12 @@ function App() {
               <Dropdown.Item onClick={() => setOctaveCount(4)}>4</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Button onClick={() => playNote()}>Play</Button>
         </Col>
       </Row>
     </Container>
--- gui/server/app.js
+++ gui/server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const http = require("http");
+const url = require("url");
 const midi = require("midi");
 var easymidi = require("easymidi");
 
 // Create Express app
 const app = express();
+
+const midiOutputName = "Mac Internal MIDI Bus 1";
+const output = new easymidi.Output(midiOutputName);
+
+let rootNote = 60;
+let octaves = 2;
 
 // Hacked delay function
 const delay = (milliseconds) => {
@@ -15,22 +23,30 @@ const delay = (milliseconds) => {
 };
 
 // A sample route
-app.get("/", (req, res) => {
-  var output = new easymidi.Output("Mac Internal MIDI Bus 1");
+app.get("/play", (req, res) => {
+  console.log("playing note");
 
-  output.send("noteon", {
-    note: 64,
-    velocity: 127,
-    channel: 1,
-  });
+  for (var i = 0; i < 1000; i++) {
+    output.send("noteon", {
+      note: 64,
+      velocity: 127,
+      channel: 1,
+    });
 
-  delay(200);
+    delay(20);
 
-  output.send("noteoff", {
-    note: 64,
-    velocity: 0,
-    channel: 1,
-  });
+    output.send("noteoff", {
+      note: 64,
+      velocity: 0,
+      channel: 1,
+    });
+  }
+});
+
+app.get("/updateRoot", (req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  rootNote = queryObject.root;
+  console.log(rootNote);
 });
 
 // Start the Express server
